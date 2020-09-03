84833257ebe24398d20b5036047df0d66f0b51b0
Vuong Tran <minhvuong.tranvan@tanchonggroup.com>
2020-09-03 23:16:27 +0700

    Integrate tailwindcss and add header

--- src/components/layouts/Header/Header.jsx
+++ src/components/layouts/Header/Header.jsx
@@ -1,9 +1,134 @@
 import React from 'react';
+import '../../../styles/main.css';
 
 export default function Header() {
   return (
-    <div className="Header">
-      <h1>Header</h1>
-    </div>
+    <header className="Header">
+      <div className="flex flex-row items-center justify-between bg-teal-500 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <svg
+            className="fill-current h-8 w-8 mr-2"
+            width="54"
+            height="54"
+            viewBox="0 0 54 54"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+          </svg>
+          <span className="font-semibold text-xl tracking-tight">
+            Học tích cực
+          </span>
+        </div>
+        <div className="relative text-gray-600">
+          <input
+            type="search"
+            name="serch"
+            placeholder="Search"
+            className="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none"
+          />
+          <button type="submit" className="absolute right-0 top-0 mt-3 mr-4">
+            <svg
+              className="h-4 w-4 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              xlinkHref="http://www.w3.org/1999/xlink"
+              version="1.1"
+              id="Capa_1"
+              x="0px"
+              y="0px"
+              viewBox="0 0 56.966 56.966"
+              width="512px"
+              height="512px"
+            >
+              <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+            </svg>
+          </button>
+        </div>
+        <a
+          href="#responsive-header"
+          className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-white mr-4"
+        >
+          Gửi câu hỏi
+        </a>
+
+        <a
+          href="#responsive-header"
+          className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-white mr-4"
+        >
+          Thông báo của tôi
+        </a>
+
+        <div className="flex flex-col">
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-white mr-4"
+          >
+            Đăng nhập
+          </a>
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-white mr-4"
+          >
+            Đăng ký
+          </a>
+        </div>
+
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="flex flex-row justify-between">
+            <a
+              href="/"
+              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            >
+              Đã lưu
+            </a>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className=" px-8 flex flex-row justify-between bg-teal-500">
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-white mr-4"
+          >
+            Hỏi đáp
+          </a>
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-white mr-4"
+          >
+            Khoá học
+          </a>
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-white"
+          >
+            Thi tự luận
+          </a>
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-white"
+          >
+            Đề thi
+          </a>
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-white"
+          >
+            Đề ra kỳ này
+          </a>
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-white"
+          >
+            Chủ đề
+          </a>
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-white"
+          >
+            Cập nhật
+          </a>
+        </div>
+      </div>
+    </header>
   );
 }
